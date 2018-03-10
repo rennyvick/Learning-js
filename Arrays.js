e01b253a75f1961679df810ed3a9a377f3c81fc0
rennyvick <35160848+rennyvick@users.noreply.github.com>
2018-03-10 13:47:34 +0300

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+var student = ["Jane","Mary","Lauren"," Ivy"," Fatma","Judith","Irene"] 
+var shoeSize =["10"," 34","6","7","44","4"]
+//get array length
+console.log(student.length)
+for (i= 0; i< student.length;i+=1)
+{
+	console.log(student[i] );
+}
---
+++ FunctionParameters.js
@@ -0,0 +1,22 @@
+function area_triangle( height , base){
+	
+	var answer = 0.5 * base * height;
+
+	return answer;
+
+}
+var answer = area_triangle(5,8);
+var answer2 = area_triangle(15,81);
+var answer4 = area_triangle(90,84);
+
+print( result)
+print(result2)
+print(result4)
+
+
+
+function print(answer) {
+  console.log(" Area of Triangle: "answer)
+}
+
+
